e2ba87079080178df4e34e481f5338219c49ecc3
Jordan Rocha <70rd23@gmail.com>
2018-06-01 09:57:02 -0300

    Adaptação dos arquivos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="wrapper">
+		<Form @getPublications="parentGetPublications" />
+		<Feed :publications="publications" @getPublications="parentGetPublications" />
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import PublicationService from '@/services/PublicationService'
+
+import Form from '@/components/Form'
+import Feed from '@/components/Feed'
 
 export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
-};
+	name: 'Home',
+	data() {
+		return {
+			publications: []
+		}
+	},
+	components: { Form, Feed },
+	mounted() {
+		this.getPublications()
+	},
+	methods: {
+		async getPublications() {
+			const response = await PublicationService.getPublications()
+
+			if (response && response.data) this.publications = response.data
+			else console.error('no lucky for you')
+		},
+
+		parentGetPublications() {
+			this.getPublications()
+		}
+	}
+}
 </script>
